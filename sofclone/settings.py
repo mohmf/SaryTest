055d9f52d220701d076e9c6b079c69df5f559da6
mohammed <mohmf3@gmail.com>
2021-08-04 15:26:14 +0300

    update k8s yamls

--- sofclone/settings.py
+++ sofclone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+kpn7l8am173e_(agf*n8z+52q1%)&wmsd!-e0d%3pffp1y_1&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','158.69.192.217']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
